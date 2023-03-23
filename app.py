fdc84a01e716fb91420d4cd36556b11b3c285f9a
Nino Kristesiashvili <Nino Kristesiashvili>
2023-03-23 20:48:46 +0400

    chaqnged port

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def detect():
         
         return jsonify(detectObjects('images/dog123.jpeg'))
 
-app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
+app.run(port=int(os.environ.get("PORT", 5000)),host='0.0.0.0',debug=True)
